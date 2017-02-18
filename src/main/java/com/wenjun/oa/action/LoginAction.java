dd62cfd9cbd9b0e946923c844913b927b3d3d0f2
deeper <352085768@qq.com>
2017-02-18 23:01:17 +0800

    first commit

---
+++ src/main/java/com/wenjun/oa/action/LoginAction.java
@@ -0,0 +1,34 @@
+package com.wenjun.oa.action;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+/**
+ * Created by Administrator on 2017/2/17.
+ */
+public class LoginAction extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+        request.setCharacterEncoding("utf-8");
+        response.setCharacterEncoding("utf-8");
+
+        String name = request.getParameter("name");
+        JSONObject object= new JSONObject();
+        try {
+            object.put("name", "小明");
+            object.put("age", 10);
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        String content = object.toString()+name;
+        response.getWriter().write(content);
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        doPost(request,response);
+    }
+}
