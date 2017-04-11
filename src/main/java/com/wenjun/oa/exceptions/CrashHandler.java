52b2b4e513bf8af13309064a89a4c62da39454ca
deeper <deeper@gmail.com>
2017-04-11 10:30:19 +0800

    add CrashHandler

--- src/main/java/com/wenjun/oa/exceptions/CrashHandler.java
+++ src/main/java/com/wenjun/oa/exceptions/CrashHandler.java
@@ -1,18 +1,17 @@
-package com.wenjun.oa.test;
+package com.wenjun.oa.exceptions;
 
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.ModelAndView;
 
 /**
- * Created by wangli0 on 2017/4/3.
+ * Created by wangli0 on 2017/4/11.
  * github https://github.com/wangli0
  * blog http://www.jianshu.com/u/79a88a044955
  * website: http://need88.com
  */
-
 @ControllerAdvice
-public class TestExceptionHandler {
+public class CrashHandler {
 
     @ExceptionHandler({Exception.class})
     public ModelAndView handleXXException(Exception ex){
@@ -22,5 +21,6 @@ public class TestExceptionHandler {
         mv.addObject("ex",ex);
         return mv;
     }
+
 
 }
